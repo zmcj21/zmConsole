0adbb3a99242d1d74ffc1057d3e47c888d7a313d
GreatDestroyerCharlie <zmcj20plusplus@gmail.com>
2021-12-23 02:36:52 +0300

    init

---
+++ include/examples/main.cpp
@@ -0,0 +1,9 @@
+﻿//Author:zmcj21
+//Date:2021/12/23
+
+#include "../zmConsole.h"
+
+int main()
+{
+    return 0;
+}
---
+++ include/zmConsole.h
@@ -0,0 +1,9 @@
+﻿//Author:zmcj21
+//Date:2021/12/23
+
+#ifndef ZM_CONSOLE_HEADER
+#define ZM_CONSOLE_HEADER
+
+//todo
+
+#endif
