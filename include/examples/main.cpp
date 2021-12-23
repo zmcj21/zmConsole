e6a7629f14a5fbf96752e5924108433204009578
Min <1669247240@qq.com>
2021-12-22 19:21:00 -0800

    update

--- include/examples/main.cpp
+++ include/examples/main.cpp
@@ -1,9 +1,13 @@
 ﻿//Author:zmcj21
 //Date:2021/12/23
 
+#define ZM_CONSOLE
 #include "../zmConsole.h"
+using namespace zmConsole;
 
 int main()
 {
+	Console::Write("hello zmConsole!");
+	Console::Read();
     return 0;
 }
--- include/zmConsole.h
+++ include/zmConsole.h
@@ -4,6 +4,69 @@
 #ifndef ZM_CONSOLE_HEADER
 #define ZM_CONSOLE_HEADER
 
+#ifndef LEN
+#define LEN(arr) (sizeof(arr) / sizeof(arr[0]))
+#endif //LEN
+
+#ifndef READ_BUF_SIZE
+#define READ_BUF_SIZE 2048
+#endif //READ_BUF_SIZE
+
+#include <string>
+
+//namespace:
+namespace zmConsole
+{
+//declaration:
+	class Console
+	{
+	public:
+		static void Write(const std::string& s);
+
+		static std::string Read();
+	};
+}
+
+//implemetation:
+#ifdef ZM_CONSOLE
+
+//namespace start
+namespace zmConsole
+{
+
+//linux:
+#ifdef __linux__
+#include <unistd.h>
+
+void Console::Write(const std::string& s)
+{
+	write(STDOUT_FILENO, s.c_str(), s.size());
+}
+
+std::string Console::Read()
+{
+	char* buf = new char[READ_BUF_SIZE];
+	
+	read(STDIN_FILENO, buf, READ_BUF_SIZE);
+
+	std::string s(buf);
+
+	delete buf;
+	
+	return s;
+}
+
+#endif //__linux__
+
+//windows:
+#ifdef _WIN32
+#include <Windows.h>
 //todo
 
-#endif
+#endif //_WIN32
+
+} //namespace end
+
+#endif //ZM_CONSOLE
+
+#endif //ZM_CONSOLE_HEADER
